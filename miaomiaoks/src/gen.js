545a444810dbb50ef3f344e7cd4ce6a1b02822c7
Meowmereo <23147015@student.hcmute.edu.vn>
2026-03-06 03:23:18 +0700

    update

--- miaomiaoks/src/gen.js
+++ miaomiaoks/src/gen.js
@@ -1,24 +1,43 @@
 load('config.js');
 
 function execute(url, page) {
-    if (page) url = page;
-
-    let response = fetch(url);
-    if (!response.ok) return null;
-    let doc = response.html();
-
-    let list = [];
-
-    // Lấy tất cả truyện trên trang
-    doc.select("div.flex.items-center.space-x-4").forEach(e => {
-        let link = e.select("a[href*='/read/']").first().attr("href");
-        let name = e.select("p.text-lg a").text();
-        let cover = e.select("img").attr("src");
-        if (cover && cover.indexOf("http") < 0) cover = BASE_URL + cover;
-        if (name && link) {
-            list.push({ name: name, link: link, cover: cover, host: BASE_URL });
-        }
+    if (!page) page = '1';
+    let response = fetch(url, {
+        method: "GET",
+        queries: {"page": page}
     });
 
-    return Response.success(list, null);
+    if (response.ok) {
+        let doc = response.html();
+
+        let next = null;
+        let nextEl = doc.select("a[href*='page=']").last();
+        if (nextEl && nextEl.text().indexOf("下一页") >= 0) {
+            next = (parseInt(page) + 1) + "";
+        }
+
+        let data = [];
+        doc.select("div.flex.items-center.space-x-4").forEach(e => {
+            let nameEl = e.select("p.text-lg a").first();
+            let linkEl = e.select("a[href*='/read/']").first();
+            let imgEl = e.select("img").first();
+
+            if (!nameEl || !linkEl) return;
+
+            let cover = imgEl ? imgEl.attr("src") : "";
+            if (cover && cover.indexOf("http") < 0) cover = BASE_URL + cover;
+
+            data.push({
+                name: nameEl.text(),
+                link: linkEl.attr("href"),
+                cover: cover,
+                description: e.select("p.text-sm.text-gray-500").first() ? e.select("p.text-sm.text-gray-500").first().text() : "",
+                host: BASE_URL
+            });
+        });
+
+        return Response.success(data, next);
+    }
+
+    return null;
 }
